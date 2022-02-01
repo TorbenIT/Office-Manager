d89c4bbde6c0164b1dc8d41ce68bc8ee8b8b9705
unknown <tobias.schneider.3@student.fhws.de>
2022-02-01 08:16:33 +0100

    added monthlyTasks sub

---
+++ monthlyTasks.vb
@@ -0,0 +1,54 @@
+Sub MonthlyTasks()
+    'Neue Daten (Datums) für neuen Monat erzeugen
+    Selection.AutoFill Destination:=Range("C2:C33"), Type:=xlFillDefault
+       
+    'Alle Wochenenden gelb markieren
+    Selection.FormatConditions.Add Type:=xlExpression, Formula1:= _
+        "=ODER(WOCHENTAG(C2)=1;WOCHENTAG(C2)=7)"
+    Selection.FormatConditions(Selection.FormatConditions.Count).SetFirstPriority
+    With Selection.FormatConditions(1).Interior
+        .PatternColorIndex = xlAutomatic
+        .Color = 65535
+        .TintAndShade = 0
+    End With
+    Selection.Borders(xlDiagonalDown).LineStyle = xlNone
+    Selection.Borders(xlDiagonalUp).LineStyle = xlNone
+    With Selection.Borders(xlEdgeLeft)
+        .LineStyle = xlContinuous
+        .ColorIndex = 0
+        .TintAndShade = 0
+        .Weight = xlThin
+    End With
+    With Selection.Borders(xlEdgeTop)
+        .LineStyle = xlContinuous
+        .ColorIndex = 0
+        .TintAndShade = 0
+        .Weight = xlThin
+    End With
+    With Selection.Borders(xlEdgeBottom)
+        .LineStyle = xlContinuous
+        .ColorIndex = 0
+        .TintAndShade = 0
+        .Weight = xlThin
+    End With
+    With Selection.Borders(xlEdgeRight)
+        .LineStyle = xlContinuous
+        .ColorIndex = 0
+        .TintAndShade = 0
+        .Weight = xlThin
+    End With
+    Selection.Borders(xlInsideVertical).LineStyle = xlNone
+    Selection.Borders(xlInsideHorizontal).LineStyle = xlNone
+    
+    Selection.FormatConditions(1).StopIfTrue = False
+    
+    
+    'Task Spalte Inhalt loeschen
+    Range("E2:E33").Select
+    Selection.ClearContents
+    With Selection.Interior
+        .Pattern = xlNone
+        .TintAndShade = 0
+        .PatternTintAndShade = 0
+    End With
+End Sub
